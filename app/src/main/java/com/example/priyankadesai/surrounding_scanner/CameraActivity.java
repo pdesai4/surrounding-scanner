63058f9e0ef9ea4d4fbf42f862b0b9da56c7d129
Priyanka Desai <pdesai4@binghamton.edu>
2018-04-27 23:59:12 -0700

    Fix Vision API Call, working now

--- app/src/main/java/com/example/priyankadesai/surrounding_scanner/CameraActivity.java
+++ app/src/main/java/com/example/priyankadesai/surrounding_scanner/CameraActivity.java
@@ -1,15 +1,22 @@
 package com.example.priyankadesai.surrounding_scanner;
 
 import android.app.Activity;
-import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.hardware.Camera;
-import android.net.Uri;
 import android.os.Bundle;
 import android.os.Environment;
+import android.support.annotation.NonNull;
+import android.util.Base64;
 import android.util.Log;
 import android.view.View;
 import android.widget.FrameLayout;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
@@ -18,12 +25,22 @@ import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.Locale;
 
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.MediaType;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.Response;
+
 public class CameraActivity extends Activity {
 
     public static final int MEDIA_TYPE_IMAGE = 1;
 
     private static final String TAG = CameraActivity.class.getName();
-    private boolean imageCaptured;
+    private static final MediaType MEDIA_TYPE_JSON = MediaType.parse("application/json; charset=utf-8");
+    private final static String API_KEY = "XXXX";
+    private OkHttpClient mOkHttpClient;
     private Camera mCamera;
     private Camera.PictureCallback mPictureCallback;
 
@@ -32,9 +49,9 @@ public class CameraActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_camera);
 
-        imageCaptured = false;
         // Create an instance of Camera
         mCamera = getCameraInstance();
+        mOkHttpClient = new OkHttpClient();
 
         // Create our Preview view and set it as the content of our activity.
         CameraPreview cameraPreview = new CameraPreview(this, mCamera);
@@ -47,31 +64,29 @@ public class CameraActivity extends Activity {
             public void onPictureTaken(byte[] data, Camera camera) {
                 File pictureFile = getOutputMediaFile(MEDIA_TYPE_IMAGE);
                 if (pictureFile == null) {
-                    Log.d(TAG, "Error creating media file, check storage permissions");
+                    Log.e(TAG, "Error creating media file, check storage permissions");
                     return;
                 }
-                imageCaptured = true;
                 try {
-                    FileOutputStream fos = new FileOutputStream(pictureFile);
-                    fos.write(data);
-                    fos.close();
+                    FileOutputStream fileOutputStream = new FileOutputStream(pictureFile);
+                    fileOutputStream.write(data);
+                    fileOutputStream.close();
+                    sendImageToCloud(pictureFile.getAbsolutePath());
                 } catch (FileNotFoundException e) {
-                    Log.d(TAG, "File not found: " + e.getMessage());
+                    Log.e(TAG, "File not found", e);
                 } catch (IOException e) {
-                    Log.d(TAG, "Error accessing file: " + e.getMessage());
+                    Log.e(TAG, "Error accessing file", e);
                 }
             }
         };
 
-        findViewById(R.id.button_capture).setOnClickListener(
-                new View.OnClickListener() {
-                    @Override
-                    public void onClick(View v) {
-                        // get an image from the camera
-                        mCamera.takePicture(null, null, mPictureCallback);
-                    }
-                }
-        );
+        findViewById(R.id.button_capture).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // get an image from the camera
+                mCamera.takePicture(null, null, mPictureCallback);
+            }
+        });
     }
 
     @Override
@@ -102,55 +117,84 @@ public class CameraActivity extends Activity {
     }
 
     /**
-     * Create a file Uri for saving an image
-     */
-    private Uri getOutputMediaFileUri(int type) {
-        return Uri.fromFile(getOutputMediaFile(type));
-    }
-
-    /**
      * Create a File for saving an image
      */
     private File getOutputMediaFile(int type) {
         // To be safe, you should check that the SDCard is mounted
         // using Environment.getExternalStorageState() before doing this.
 
-        File mediaStorageDir = new File(Environment.getExternalStoragePublicDirectory(
-                Environment.DIRECTORY_PICTURES), "MyCameraApp");
+        File mediaStorageDir = new File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES),
+                getString(R.string.app_name));
         // This location works best if you want the created images to be shared
         // between applications and persist after your app has been uninstalled.
 
         // Create the storage directory if it does not exist
         if (!mediaStorageDir.exists()) {
             if (!mediaStorageDir.mkdirs()) {
-                Log.d("MyCameraApp", "failed to create directory");
+                Log.e(TAG, "failed to create directory");
                 return null;
             }
         }
 
         // Create a media file name
         String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss", Locale.US).format(new Date());
-        File mediaFile;
         if (type == MEDIA_TYPE_IMAGE) {
-            mediaFile = new File(mediaStorageDir.getPath() + File.separator +
+            return new File(mediaStorageDir.getPath() + File.separator +
                     "IMG_" + timeStamp + ".jpg");
-        } else {
-            return null;
         }
-
-        return mediaFile;
+        return null;
     }
 
-    @Override
-    public void onBackPressed() {
-        super.onBackPressed();
-        Intent intent;
-        if(!imageCaptured) {
-            intent = new Intent(this, MainActivity.class);
-        } else {
-            intent =new Intent(this, CameraActivity.class);
-        }
-        startActivity(intent);
-    }
+    private void sendImageToCloud(String fileUri) {
+        //createServiceAccount();
+        Bitmap bitmap = BitmapFactory.decodeFile(fileUri);
+        ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+        bitmap.compress(Bitmap.CompressFormat.JPEG, 100, byteArrayOutputStream);
+        byte[] byteArrayImage = byteArrayOutputStream.toByteArray();
+        String encodedImage = Base64.encodeToString(byteArrayImage, Base64.DEFAULT);
 
+        JSONObject jsonObjectBody = new JSONObject();
+        try {
+            JSONArray jsonArrayRequests = new JSONArray();
+            JSONObject jsonObjectRequest = new JSONObject();
+
+            JSONObject jsonObjectImage = new JSONObject();
+            jsonObjectImage.put("content", encodedImage);
+
+            JSONArray jsonArrayFeatures = new JSONArray();
+            JSONObject jsonObjectFeature = new JSONObject();
+            jsonObjectFeature.put("type", "LABEL_DETECTION");
+            jsonObjectFeature.put("maxResults", "1");
+            jsonArrayFeatures.put(jsonObjectFeature);
+
+            jsonObjectRequest.put("image", jsonObjectImage);
+            jsonObjectRequest.put("features", jsonArrayFeatures);
+
+            jsonArrayRequests.put(jsonObjectRequest);
+
+            jsonObjectBody.put("requests", jsonArrayRequests);
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+        Log.d(TAG, "Request body: " + jsonObjectBody.toString());
+        String url = "https://vision.googleapis.com/v1/images:annotate?key=" + API_KEY;
+        RequestBody body = RequestBody.create(MEDIA_TYPE_JSON, jsonObjectBody.toString());
+        final Request request = new Request.Builder()
+                .url(url)
+                .post(body)
+                .build();
+        mOkHttpClient.newCall(request).enqueue(new Callback() {
+            @Override
+            public void onFailure(@NonNull Call call, @NonNull IOException e) {
+                Log.e(TAG, "API call failed", e);
+            }
+
+            @Override
+            public void onResponse(@NonNull Call call, @NonNull Response response) throws IOException {
+                Log.d(TAG, "Response code: " + response.code());
+                Log.d(TAG, "Response: " + response.body().string());
+            }
+        });
+    }
 }
